04610d1ec0a46df1cdacfee94fc7e34a462607d0
Anandaplus@1 <shayanchatterjee7@gmail.com>
2021-04-13 15:12:15 +0530

    Android Product flavour integration

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
+import config from 'react-native-config';
 
 const App = () => (
   <SafeAreaView>
     <Text>Initial setup</Text>
+    <Text>{config.API_BASE_URL}</Text>
   </SafeAreaView>
 );
 
