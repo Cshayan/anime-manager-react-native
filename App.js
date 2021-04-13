a8e35252ab80c2ca90509901a6127e5131b49d9e
Anandaplus@1 <shayanchatterjee7@gmail.com>
2021-04-13 12:03:47 +0530

    Initial project setup

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Initial setup</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
